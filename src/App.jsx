b6415616a664451d3202b706efdc486c6fe2b4f7
SaGerz <genarosamuel24@gmail.com>
2026-01-11 21:04:19 +0700

    feat: add routing login basic

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>  
+    <Routes>
+      <Route path='/login' element={ <Login /> } />
+    </Routes>
   )
 }
 
