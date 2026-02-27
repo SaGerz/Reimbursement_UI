5c8b2cd11f4d35beb488185c5504c9e38d9eb806
SaGerz <genarosamuel24@gmail.com>
2026-02-27 21:54:43 +0700

    fix: Refactor Navbar to Components

--- src/layouts/EmployeeLayout.jsx
+++ src/layouts/EmployeeLayout.jsx
@@ -1,25 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
+import EmployeSidebar from "../components/sidebar/EmployeSidebar";
 
 const EmployeeLayout = () => {
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
       <aside className="w-64 bg-gray-800 text-white p-4">
-        <h1 className="font-bold text-lg">Employee</h1>
-
-        <nav className="mt-4 space-y-2 flex flex-col">
-          <Link to="/employee/dashboard" className="hover:underline">
-            Dashboard
-          </Link>
-
-          <Link to="/employee/reimburstment" className="hover:underline">
-            My Reimbursements
-          </Link>
-
-          <Link to="/employee/reimburstment/new" className="hover:underline">
-            Submit Reimbursement
-          </Link>
-        </nav>
+        <EmployeSidebar />
       </aside>
 
       {/* Main */}
